078ba55302f3696e7bf0566400d1d4db45438b50
abhi <abhideepak22@gmail.com>
2020-08-09 13:23:49 +0530

    Updates

---
+++ precache-manifest.571877df38330ce28042d07e631d82d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f41e744700ad1ddb859db286666c97",
+    "url": "/truckhome/index.html"
+  },
+  {
+    "revision": "b55e4bcc96040bf7f7a8",
+    "url": "/truckhome/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5270a39ebad7adad9ff4",
+    "url": "/truckhome/static/css/main.8b076cef.chunk.css"
+  },
+  {
+    "revision": "b55e4bcc96040bf7f7a8",
+    "url": "/truckhome/static/js/2.ee436aa6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/truckhome/static/js/2.ee436aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5270a39ebad7adad9ff4",
+    "url": "/truckhome/static/js/main.05ff4527.chunk.js"
+  },
+  {
+    "revision": "2b3e2f5e3fbb775dd13d",
+    "url": "/truckhome/static/js/runtime-main.f17e446b.js"
+  }
+]);
